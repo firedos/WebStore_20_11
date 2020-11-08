708b8e75f8d576dd6954f185576fd76b804261e4
Sergey Kiselev <fireados@outlook.com>
2020-11-08 15:29:03 +0300

    пример динамического чтения конфигурации

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text;
 
 namespace WebStore
 {
@@ -30,11 +31,15 @@ namespace WebStore
 
             app.UseRouting();
 
+            var utf8 = Encoding.UTF8;
+            string greet = Configuration["CustomGreetings"];
+            byte[] utfBytes = utf8.GetBytes(greet);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(Configuration["CustomGreetings"]);
                 });
             });
         }
