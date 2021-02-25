1aadc2c647221235e3709c659c694e3066defc07
Sergey Kiselev <fireados@outlook.com>
2021-02-25 23:14:01 +0300

    Внедрение зависимости в конроллер сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -4,22 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     //[Route("users")]
-    public class EmployeesController : Controller
+    class EmployeesController : Controller
     {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesController(IEmployeesData employeesData)
+        {
+            _EmployeesData = employeesData;
+        }
+
         //[Route("employees")]
-        public IActionResult Index() => View(TestData.Employees);
+        public IActionResult Index() => View(_EmployeesData.GetAll());
 
         //[Route("employee/{id}")]
         /// <summary>карточка отдельного сотрудника</summary>
         /// <returns></returns>
         public IActionResult Details(int Id)
         {
-            var employees = TestData.Employees.FirstOrDefault(e => e.Id == Id);
+            //var employees = TestData.Employees.FirstOrDefault(e => e.Id == Id);
+            var employees = _EmployeesData.GetById(Id);
             if (employees == null)
             {
                 return NotFound();
